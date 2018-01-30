cd4dc103bf2cd96321fe26e1bf99d3cc1cc25390
shubhamknoldus <shubham.verma@knoldus.in>
2018-01-30 23:41:12 +0530

    final commit

--- src/main/scala/edu/operation/FileOperations.scala
+++ src/main/scala/edu/operation/FileOperations.scala
@@ -10,20 +10,20 @@ class FileOperations {
   def printFileStructure(sourceFolder: File): Future[String] = Future {
     val stringPath: String = sourceFolder.getAbsolutePath
 
-    val hof = (dirObj: File) => dirObj.listFiles().toList.partition(file => !file.isFile)
+    val directoryAndFilesSplitter = (dirObj: File) => dirObj.listFiles().toList.partition(file => !file.isFile)
 
-    val (dList, fList) = hof(sourceFolder)
+    val (dList, fList) = directoryAndFilesSplitter(sourceFolder)
 
     def printFileStructureHelper(dirList: List[File], fileList: List[File]): List[File] = {
       dirList match {
-        case head :: Nil => val (a, b) = hof(head)
+        case head :: Nil => val (a, b) = directoryAndFilesSplitter(head)
           if (a.isEmpty) {
             b ::: fileList
           } else {
             printFileStructureHelper(dirList.drop(1) ::: a, fileList ::: b)
           }
         case head :: head2 :: tail =>
-          val (a, b) = hof(head)
+          val (a, b) = directoryAndFilesSplitter(head)
           if (a.isEmpty) {
             printFileStructureHelper(dirList.drop(1), fileList ::: b)
           } else {
